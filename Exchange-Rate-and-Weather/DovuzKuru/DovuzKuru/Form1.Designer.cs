a6bc094afada5079916e90d23841e3f264394bd1
EnesKaleci <eneskaleci2@gmail.com>
2024-02-28 01:24:56 +0300

    Exchange-Rate-and-Weather-API

---
+++ Exchange-Rate-and-Weather/DovuzKuru/DovuzKuru/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace DovuzKuru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblDolar = new System.Windows.Forms.Label();
+            this.lblEuro = new System.Windows.Forms.Label();
+            this.lblSterlin = new System.Windows.Forms.Label();
+            this.lbljpn = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sterlin = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblWindSpeed = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(98, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblDolar
+            // 
+            this.lblDolar.AutoSize = true;
+            this.lblDolar.Location = new System.Drawing.Point(136, 73);
+            this.lblDolar.Name = "lblDolar";
+            this.lblDolar.Size = new System.Drawing.Size(35, 13);
+            this.lblDolar.TabIndex = 1;
+            this.lblDolar.Text = "label1";
+            // 
+            // lblEuro
+            // 
+            this.lblEuro.AutoSize = true;
+            this.lblEuro.Location = new System.Drawing.Point(136, 99);
+            this.lblEuro.Name = "lblEuro";
+            this.lblEuro.Size = new System.Drawing.Size(35, 13);
+            this.lblEuro.TabIndex = 1;
+            this.lblEuro.Text = "label1";
+            // 
+            // lblSterlin
+            // 
+            this.lblSterlin.AutoSize = true;
+            this.lblSterlin.Location = new System.Drawing.Point(136, 127);
+            this.lblSterlin.Name = "lblSterlin";
+            this.lblSterlin.Size = new System.Drawing.Size(35, 13);
+            this.lblSterlin.TabIndex = 1;
+            this.lblSterlin.Text = "label1";
+            // 
+            // lbljpn
+            // 
+            this.lbljpn.AutoSize = true;
+            this.lbljpn.Location = new System.Drawing.Point(136, 153);
+            this.lbljpn.Name = "lbljpn";
+            this.lbljpn.Size = new System.Drawing.Size(35, 13);
+            this.lbljpn.TabIndex = 1;
+            this.lbljpn.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dolar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Euro";
+            // 
+            // sterlin
+            // 
+            this.sterlin.AutoSize = true;
+            this.sterlin.Location = new System.Drawing.Point(95, 127);
+            this.sterlin.Name = "sterlin";
+            this.sterlin.Size = new System.Drawing.Size(36, 13);
+            this.sterlin.TabIndex = 1;
+            this.sterlin.Text = "Sterlin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(95, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "JPY";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(92, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Click";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(72, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(72, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label3";
+            // 
+            // lblWindSpeed
+            // 
+            this.lblWindSpeed.AutoSize = true;
+            this.lblWindSpeed.Location = new System.Drawing.Point(72, 73);
+            this.lblWindSpeed.Name = "lblWindSpeed";
+            this.lblWindSpeed.Size = new System.Drawing.Size(35, 13);
+            this.lblWindSpeed.TabIndex = 6;
+            this.lblWindSpeed.Text = "label3";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lbljpn);
+            this.panel1.Controls.Add(this.sterlin);
+            this.panel1.Controls.Add(this.lblSterlin);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lblEuro);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblDolar);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(34, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(277, 322);
+            this.panel1.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(77, 40);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(160, 20);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "EXCHANGE RATE";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.lblWindSpeed);
+            this.panel2.Location = new System.Drawing.Point(424, 37);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(267, 329);
+            this.panel2.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(71, 40);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 20);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "WEATHER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblDolar;
+        private System.Windows.Forms.Label lblEuro;
+        private System.Windows.Forms.Label lblSterlin;
+        private System.Windows.Forms.Label lbljpn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label sterlin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblWindSpeed;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Exchange-Rate-and-Weather/DovuzKuru/DovuzKuru/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DovuzKuru
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //!#! CHANGE THIS IN TWO PROJECTS
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                XmlDocument xmlVerisi = new XmlDocument();
+                xmlVerisi.Load("http://www.tcmb.gov.tr/kurlar/today.xml");
+
+                decimal dolar = Convert.ToDecimal(xmlVerisi.SelectSingleNode(string.Format("Tarih_Date/Currency[@Kod='{0}']/ForexSelling", "USD")).InnerText.Replace('.', ','));
+                decimal euro = Convert.ToDecimal(xmlVerisi.SelectSingleNode(string.Format("Tarih_Date/Currency[@Kod='{0}']/ForexSelling", "EUR")).InnerText.Replace('.', ','));
+                decimal sterlin = Convert.ToDecimal(xmlVerisi.SelectSingleNode(string.Format("Tarih_Date/Currency[@Kod='{0}']/ForexSelling", "GBP")).InnerText.Replace('.', ','));
+                decimal jpn = Convert.ToDecimal(xmlVerisi.SelectSingleNode(string.Format("Tarih_Date/Currency[@Kod='{0}']/ForexSelling", "JPY")).InnerText.Replace('.', ','));
+                //decimal # = Convert.ToDecimal(xmlVerisi.SelectSingleNode(string.Format("Tarih_Date/Currency[@Kod='{0}']/ForexSelling", "#")).InnerText.Replace('.', ','));
+                lblDolar.Text = dolar.ToString();
+                lblEuro.Text = euro.ToString();
+                lblSterlin.Text = sterlin.ToString();
+                lbljpn.Text = jpn.ToString();
+
+            }
+            catch (XmlException xml)
+            {
+                
+                MessageBox.Show(xml.ToString());
+            }
+
+
+
+
+
+
+        }
+
+        private const string API_KEY = "8d38fe5bfde0f53cb48bc740bf9ab659";
+        // YOUR CITY CODE = #
+        private const string CITY_ID = "#";
+        private const string API_URL = "http://api.openweathermap.org/data/2.5/weather?id={0}&appid={1}&units=metric";
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            UpdateWeather();
+
+
+
+        }
+
+        private void UpdateWeather()
+        {
+            string apiUrl = string.Format(API_URL, CITY_ID, API_KEY);
+
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    string json = client.DownloadString(apiUrl);
+                    dynamic data = JObject.Parse(json);
+
+                    
+                    label3.Text = string.Format("Sıcaklık: {0} °C", data.main.temp);
+                    label5.Text = string.Format("Hissedilen Sıcaklık: {0} °C", data.main.feels_like);
+                    label6.Text = string.Format("Hava Durumu: {0}", data.weather[0].description);
+                    lblWindSpeed.Text = string.Format("Rüzgar Hızı: {0} m/s", data.wind.speed);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hava durumu verileri alınamadı. Hata: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
